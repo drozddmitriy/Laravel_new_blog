774768f9e6b2c11be74d539795eb49e19f78cd90
drozddmitriy <drozd.dmitriy.etc@gmail.com>
2018-10-23 11:16:04 +0300

    CRUD for User

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Storage;
@@ -19,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
 
     /**
@@ -55,7 +56,6 @@ class User extends Authenticatable
     public static function add($fields){
         $user = new static;
         $user->fill($fields);
-        $user->password = bcrypt($fields['password']);
         $user->save();
 
         return $user;
@@ -67,15 +67,26 @@ class User extends Authenticatable
      */
     public function edit($fields){
         $this->fill($fields);
-        $this->password = bcrypt($fields['password']);
         $this->save();
     }
+
+    /**
+     * Generate password
+     * @param $password
+     */
+    public function generatePassword($password){
+        if($password != null) {
+            $this->password = bcrypt($password);
+            $this->save();
+        }
+    }
+
 
     /**
      * Delete user
      */
     public function remove(){
-        Storage::delete('uploads/'. $this->image);
+        $this->removeAvatar();
         $this->delete();
     }
 
@@ -87,11 +98,18 @@ class User extends Authenticatable
 
         if($image == null){return;}
 
-        Storage::delete('uploads/'. $this->image);
+        $this->removeAvatar();
+
         $filename = str_random(10). '.' . $image->extension();
-        $image->saveAs('uploads', $filename);
-        $this->image = $filename;
+        $image->storeAs('uploads', $filename);
+        $this->avatar = $filename;
         $this->save();
+    }
+
+    public function removeAvatar(){
+        if($this->avatar != null) {
+            Storage::delete('uploads/' . $this->avatar);
+        }
     }
 
     /**
@@ -99,10 +117,10 @@ class User extends Authenticatable
      * @return string
      */
     public function getAvatar(){
-        if($this->image == null) {
-            return '/img/no-user-image.png';
+        if($this->avatar == null) {
+            return '/img/no-image.png';
         }
-        return '/uploads/' . $this->image;
+        return '/uploads/' . $this->avatar;
     }
 
     /**
