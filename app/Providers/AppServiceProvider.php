566ec35d2c877e6528aba0e0af6a58856bb2e08c
drozddmitriy <drozd.dmitriy.etc@gmail.com>
2018-10-25 23:16:00 +0300

    create_sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Post;
+use App\Category;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -15,6 +17,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        view()->composer('pages._sidebar', function($view){
+            $view->with('popularPosts', Post::getPopularPosts());
+            $view->with('featuredPosts', Post::where('is_featured', 1)->take(3)->get());
+            $view->with('recentPosts', Post::orderBy('date', 'desc')->take(4)->get());
+            $view->with('categories', Category::all());
+        });
     }
 
     /**
